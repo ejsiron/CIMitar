2e7726cb88fef81dc888d09711d412c8352a186d
Eric Siron <ejsiron@users.noreply.github.com>
2020-11-27 22:36:58 -0600

    element updates

--- CIMitar/element.cpp
+++ CIMitar/element.cpp
@@ -3,6 +3,8 @@
 
 CIMitar::Element::Element(const MI_Char* Name, MI_Value* Value, MI_Type Type, MI_Uint32 MemoryFlag)
 {
+	int ack;
+
 }
 
 CIMitar::Element::Element(std::wstring& Name, MI_Value& Value, MI_Type Type, MI_Uint32 MemoryFlag)
